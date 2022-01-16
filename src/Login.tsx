742f619614ccc70ca5e50c30564e8e03b31ff693
NM-2000 <nikhilmittal3434@gmail.com>
2022-01-16 14:28:20 +0530

    Login Page Initial Code

---
+++ src/Login.tsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Login: React.FC<{}> = ({}) => {
+
+    return <></>;
+
+};
+
+export default memo(Login);
