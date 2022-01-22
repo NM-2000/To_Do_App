457cd4a72c8927d0c333f54d143c0dc8c634b9e2
NM-2000 <nikhilmittal3434@gmail.com>
2022-01-22 17:30:07 +0530

    Pressable Pressed Effects

--- src/Register.tsx
+++ src/Register.tsx
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 32,
         alignItems: 'center',
         justifyContent: 'space-between',
-        backgroundColor: 'rgb(30,30,30)'
+        backgroundColor: 'rgb(30,30,30)',
     },
     topContainerStyle: {
         width: '100%',
@@ -20,13 +20,13 @@ const styles = StyleSheet.create({
         color: 'white',
         marginBottom: 16,
         fontFamily: 'OpenSans',
-        fontWeight: '900'
+        fontWeight: '900',
     },
     fillFormTextStyle: {
         fontSize: 14,
         color: 'grey',
         fontFamily: 'OpenSans',
-        fontWeight: '900'
+        fontWeight: '900',
     },
     middleContainerStyle: {
         width: '100%',
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     signInContainer: {
-        flexDirection: 'row'
+        flexDirection: 'row',
     },
     fullNameContainerStyle: {
         flexDirection: 'row',
@@ -78,19 +78,22 @@ const styles = StyleSheet.create({
         marginHorizontal: 8,
         fontSize: 14,
         fontFamily: 'OpenSans',
-        fontWeight: '900'
+        fontWeight: '900',
     },
     havingAccountTextStyle: {
         fontSize: 14,
         color: 'white',
         fontFamily: 'OpenSans',
-        fontWeight: '900'
+        fontWeight: '900',
     },
     signInTextStyle: {
         fontSize: 14,
         color: 'rgb(105,112,255)',
         fontFamily: 'OpenSans',
-        fontWeight: '900'
+        fontWeight: '900',
+    },
+    pressedSignInTextStyle: {
+        color: 'rgb(50,100,255)',
     },
     signUpContainerStyle: {
         backgroundColor: 'rgb(105,112,255)',
@@ -101,89 +104,113 @@ const styles = StyleSheet.create({
         paddingVertical: 19,
         borderRadius: 16,
     },
+    pressedSignUpContainerStyle: {
+        backgroundColor: 'rgb(50,100,255)',
+    },
     signUpTextStyle: {
         fontFamily: 'OpenSans',
         fontWeight: '900',
-        color: 'white'
-    }
+        color: 'white',
+    },
 });
 
-const CREATE_NEW_ACCOUNT_TEXT = "Create new account";
-const FILL_FORM_TEXT = "Please fill in the form to continue";
-const FULL_NAME_PLACEHOLDER_TEXT = "Full Name";
-const EMAIL_ADDRESS_PLACEHOLDER_TEXT = "Email Address";
-const PHONE_NUMBER_PLACEHOLDER_TEXT = "Phone Number";
-const PASSWORD_PLACEHOLDER_TEXT = "Password";
-const SIGN_UP_TEXT = "Sign Up";
-const HAVE_ACCOUNT_TEXT = "Have an Account? ";
-const SIGN_IN_TEXT = "Sign In"
+const CREATE_NEW_ACCOUNT_TEXT = 'Create new account';
+const FILL_FORM_TEXT = 'Please fill in the form to continue';
+const FULL_NAME_PLACEHOLDER_TEXT = 'Full Name';
+const EMAIL_ADDRESS_PLACEHOLDER_TEXT = 'Email Address';
+const PHONE_NUMBER_PLACEHOLDER_TEXT = 'Phone Number';
+const PASSWORD_PLACEHOLDER_TEXT = 'Password';
+const SIGN_UP_TEXT = 'Sign Up';
+const HAVE_ACCOUNT_TEXT = 'Have an Account? ';
+const SIGN_IN_TEXT = 'Sign In';
 
-const Register: React.FC<{}> = ({}) => {
-
+const Register: React.FC<{}> = ({ }) => {
     const [showPassword, setShowPassword] = useState(false);
 
     const onSignInPress = useCallback(() => {
-        console.log("Sign In!");
+        console.log('Sign In!');
     }, []);
 
     const onSignUpPress = useCallback(() => {
-        console.log("Sign Up!");
+        console.log('Sign Up!');
     }, []);
 
     const togglePassword = useCallback(() => {
-        console.log("Toggle Password!");
+        console.log('Toggle Password!');
         setShowPassword(!showPassword);
     }, []);
 
     return (
-        <View style={styles.registerContainer} >
-            <View style={styles.topContainerStyle} >
-                <Text style={styles.createAccountTextStyle} >
-                    { CREATE_NEW_ACCOUNT_TEXT }
+        <View style={styles.registerContainer}>
+            <View style={styles.topContainerStyle}>
+                <Text style={styles.createAccountTextStyle}>
+                    {CREATE_NEW_ACCOUNT_TEXT}
                 </Text>
-                <Text style={styles.fillFormTextStyle} >
-                    { FILL_FORM_TEXT }
-                </Text>
+                <Text style={styles.fillFormTextStyle}>{FILL_FORM_TEXT}</Text>
             </View>
-            <View style={styles.middleContainerStyle} >
-                <View style={styles.fullNameContainerStyle} >
+            <View style={styles.middleContainerStyle}>
+                <View style={styles.fullNameContainerStyle}>
                     <FontAwesome5 name="user" size={20} />
-                    <TextInput style={styles.textInputStyle} placeholder={FULL_NAME_PLACEHOLDER_TEXT} />
+                    <TextInput
+                        style={styles.textInputStyle}
+                        placeholder={FULL_NAME_PLACEHOLDER_TEXT}
+                    />
                 </View>
-                <View style={styles.emailContainerStyle} >
+                <View style={styles.emailContainerStyle}>
                     <FontAwesome5 name="paper-plane" size={20} />
-                    <TextInput style={styles.textInputStyle} placeholder={EMAIL_ADDRESS_PLACEHOLDER_TEXT} />
+                    <TextInput
+                        style={styles.textInputStyle}
+                        placeholder={EMAIL_ADDRESS_PLACEHOLDER_TEXT}
+                    />
                 </View>
-                <View style={styles.phoneContainerStyle} >
+                <View style={styles.phoneContainerStyle}>
                     <FontAwesome5 name="phone" size={20} />
-                    <TextInput style={styles.textInputStyle} placeholder={PHONE_NUMBER_PLACEHOLDER_TEXT} />
+                    <TextInput
+                        style={styles.textInputStyle}
+                        placeholder={PHONE_NUMBER_PLACEHOLDER_TEXT}
+                    />
                 </View>
-                <View style={styles.passwordContainerStyle} >
+                <View style={styles.passwordContainerStyle}>
                     <FontAwesome5 name="lock" size={20} />
-                    <TextInput style={styles.textInputStyle} placeholder={PASSWORD_PLACEHOLDER_TEXT} secureTextEntry={showPassword ? false: true} />
-                    <Pressable style={{transform: [{rotateY: '180deg'}]}} onPress={togglePassword} >
-                        <FontAwesome5 name={ !showPassword ? "eye-slash" : "eye"} size={20}  />
+                    <TextInput
+                        style={styles.textInputStyle}
+                        placeholder={PASSWORD_PLACEHOLDER_TEXT}
+                        secureTextEntry={showPassword ? false : true}
+                    />
+                    <Pressable style={{}} onPress={togglePassword}>
+                        <FontAwesome5
+                            name={!showPassword ? 'eye-slash' : 'eye'}
+                            size={20}
+                        />
                     </Pressable>
                 </View>
             </View>
-            <View style={styles.bottomContainerStyle} >
-                <Pressable style={styles.signUpContainerStyle} onPress={onSignUpPress} >
-                    <Text style={styles.signUpTextStyle} >
-                        { SIGN_UP_TEXT }
-                    </Text>
+            <View style={styles.bottomContainerStyle}>
+                <Pressable
+                    style={({ pressed }) => [
+                        styles.signUpContainerStyle,
+                        pressed ? styles.pressedSignUpContainerStyle : {},
+                    ]}
+                    onPress={onSignUpPress}>
+                    <Text style={styles.signUpTextStyle}>{SIGN_UP_TEXT}</Text>
                 </Pressable>
-                <View style={styles.signInContainer} >
-                    <Text style={styles.havingAccountTextStyle} >
-                        { HAVE_ACCOUNT_TEXT }
-                    </Text>
-                    <Text style={styles.signInTextStyle} onPress={onSignInPress} >
-                        { SIGN_IN_TEXT }
-                    </Text>
+                <View style={styles.signInContainer}>
+                    <Text style={styles.havingAccountTextStyle}>{HAVE_ACCOUNT_TEXT}</Text>
+                    <Pressable onPress={onSignInPress}>
+                        {({ pressed }) => (
+                            <Text
+                                style={[
+                                    styles.signInTextStyle,
+                                    pressed ? styles.pressedSignInTextStyle : {},
+                                ]}>
+                                {SIGN_IN_TEXT}
+                            </Text>
+                        )}
+                    </Pressable>
                 </View>
             </View>
         </View>
     );
-
 };
 
 export default memo(Register);
