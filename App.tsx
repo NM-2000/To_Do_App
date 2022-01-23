f8cdf56bdc995c2417aac87b5b806c810279626f
NM-2000 <nikhilmittal3434@gmail.com>
2022-01-23 15:24:24 +0530

    Animation for screen push/pop

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Login, Register } from './src';
 import { ROUTES } from './routes';
 import { ROUTE_MAP } from './routeMap';
 
@@ -15,12 +14,12 @@ const App = () => {
         <Stack.Screen
           name={ROUTES.LOGIN}
           component={ROUTE_MAP[ROUTES.LOGIN]}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, animation: 'slide_from_right' }}
         />
         <Stack.Screen
           name={ROUTES.REGISTER}
           component={ROUTE_MAP[ROUTES.REGISTER]}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, animation: 'slide_from_right' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
