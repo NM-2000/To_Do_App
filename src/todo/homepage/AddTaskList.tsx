a500df3b90a7c9b813e9c5faab6d3f53e3506be2
NM-2000 <nikhilmittal3434@gmail.com>
2022-01-26 17:50:32 +0530

    Implemented Add Task List Component

---
+++ src/todo/homepage/AddTaskList.tsx
@@ -0,0 +1,53 @@
+import { NativeStackNavigationHelpers } from '@react-navigation/native-stack/lib/typescript/src/types';
+import React, { memo, useCallback } from 'react';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const styles = StyleSheet.create({
+    addTaskListContainerStyle: {
+        alignItems: 'center',
+    },
+    addTaskListButtonStyle: {
+        paddingVertical: 16,
+        paddingHorizontal: 18,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: 'grey',
+    },
+    pressedAddTaskListButtonStyle: {
+        backgroundColor: 'rgb(50,50,50)',
+    },
+    addTaskListTextStyle: {
+        fontSize: 14,
+        color: 'grey',
+        fontFamily: 'OpenSans',
+        fontWeight: "900",
+        marginTop: 16,
+    },
+});
+
+const ADD_TASK_LIST_TEXT = "Add List";
+
+const AddTaskList: React.FC<{ navigation: NativeStackNavigationHelpers }> = ({ navigation }) => {
+
+    const onAddListPress = useCallback(() => {
+        console.log("Added Task List!");
+    }, []);
+
+    return (
+        <View style={styles.addTaskListContainerStyle} >
+            <Pressable style={({ pressed }) => [styles.addTaskListButtonStyle, pressed ? styles.pressedAddTaskListButtonStyle : {}]} onPress={onAddListPress} >
+                <FontAwesome5
+                    name={"plus"}
+                    size={20}
+                />
+            </Pressable>
+            <Text style={styles.addTaskListTextStyle} >
+                { ADD_TASK_LIST_TEXT }
+            </Text>
+        </View>
+    );
+
+};
+
+export default memo(AddTaskList);
--- src/todo/homepage/HomePage.tsx
+++ src/todo/homepage/HomePage.tsx
@@ -1,13 +1,15 @@
 import { NativeStackNavigationHelpers } from '@react-navigation/native-stack/lib/typescript/src/types';
 import React, { memo } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AddTaskList from './AddTaskList';
 import HomePageTitle from './HomePageTitle';
 
 const styles = StyleSheet.create({
     homepageContainerStyle: {
         flex: 1,
         backgroundColor: 'rgb(30,30,30)',
-        paddingTop: 100,
+        alignItems: 'center',
+        justifyContent: 'space-around'
     }
 });
 
@@ -25,6 +27,7 @@ const HomePage: React.FC<{ navigation: NativeStackNavigationHelpers }> = ({ navi
                 /**
                  * Add Task List Implementation / Static Section
                  */
+                <AddTaskList navigation={navigation} />
             }
             {
                 /**
